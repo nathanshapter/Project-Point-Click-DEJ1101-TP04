d4afc459631f24274274b96c31edff8d88b7738f
Nathan Shapter <shapternathan@protonmail.com>
2025-04-17 11:59:09 -0400

    added dotween and sound manager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    [Header("Sources")]
+    public AudioSource musicSource;
+    public AudioSource sfxSource;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else { Destroy(gameObject); }
+    }
+    public void PlaySFX(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
+    }
+
+    public void PlayMusic(AudioClip clip, bool loop = true)
+    {
+        musicSource.clip = clip;
+        musicSource.loop = true;
+        musicSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        musicSource.Stop();
+    }
+
+}
